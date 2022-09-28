366c368fe5a47f1ab65782c9093ca99fc8772a66
mylearningsre <mylearningsre@gmail.com>
2022-09-28 12:04:21 +0530

    Modified addition logic to division

--- Addition.java
+++ Addition.java
@@ -4,12 +4,13 @@ class Addition
         {
                 int num1,num2,res;
                 Scanner s = new Scanner(System.in);
+		System.out.println("This is stage branch code");
                 System.out.println("welcome to Devops GC{P course");
 		System.out.println("Enter num1 value");
                 num1=s.nextInt();
                 System.out.println("Enter num2 value");
                 num2=s.nextInt();
-                res=num1+num2;
+                res=num1/num2;
                 System.out.println("The result is "+res);
                 System.out.println"Thank You");
         }
