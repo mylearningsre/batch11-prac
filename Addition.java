27ba4caf509f9a3cd1ce3403e1fc537f514e1130
mylearningsre <mylearningsre@gmail.com>
2022-09-29 21:06:55 +0530

    Added product

--- Addition.java
+++ Addition.java
@@ -5,12 +5,15 @@ class Addition
                 int num1,num2,res;
                 Scanner s = new Scanner(System.in);
                 System.out.println("welcome to Devops GC{P course");
+		System.out.println("This is Prod branch code");
 		System.out.println("Enter num1 value");
                 num1=s.nextInt();
                 System.out.println("Enter num2 value");
                 num2=s.nextInt();
                 res=num1+num2;
                 System.out.println("The result is "+res);
+		res=num1*num2;
+		System.out.println("The Product result is "+res);
                 System.out.println"Thank You");
         }
 }
